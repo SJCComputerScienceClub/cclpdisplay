781e7bacac44cb8201bf64e7db6e7f91e54c7654
Ldsouza28 <ldsouza28@stjohnschs.org>
2026-03-20 10:00:27 -0400

    Inserting JS

---
+++ trivia.js
@@ -0,0 +1,11 @@
+const questionOneSection = document.querySelector(".questionOneSection");
+
+const triviaQuestions = {
+    questionOne: {
+        option1: "1776",
+        option2: "1991",
+        option3: "1915",
+        option4: "1851",
+    }
+};
+
